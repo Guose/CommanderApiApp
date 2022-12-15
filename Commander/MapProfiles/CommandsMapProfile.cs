a4244245a83a3d753f7da42cb0a85e580fbee8b8
Justin Elder <guose79@gmail.com>
2022-12-14 22:25:34 -0800

    Added mapping

--- Commander/MapProfiles/CommandsMapProfile.cs
+++ Commander/MapProfiles/CommandsMapProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Commander.Dtos;
 using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Commander.Profiles
 {
@@ -9,6 +10,7 @@ namespace Commander.Profiles
         public CommandsMapProfile()
         {
             CreateMap<Command, CommandReadDto>();
+            CreateMap<ActionResult, Command>();
             CreateMap<CommandCreateDto, Command>();
             CreateMap<CommandUpdateDto, Command>();
             CreateMap<Command, CommandUpdateDto>();
