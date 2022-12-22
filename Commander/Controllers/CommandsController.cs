e2a90ae56b6a567169688708e04d30cc04ae5c73
Justin Elder <justin.elder@basecapanalytics.com>
2022-12-21 19:05:20 -0800

    Added Mapping class

--- Commander/Controllers/CommandsController.cs
+++ Commander/Controllers/CommandsController.cs
@@ -4,7 +4,6 @@ using Commander.Dtos;
 using Commander.Models;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
